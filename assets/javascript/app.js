9866413a5576c131b32938eac3d7fdec85d74a9c
FyperTheViper <b.w.byars@gmail.com>
2018-10-18 17:26:49 -0700

    opening moves

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+/*
+Need to declare variables, a JSON object containing all the questions each with [question string, array of answers, correct answer, and any image assets]
+Other variables - counter timer, answerRight, answerWrong, and an undefined variable that will populate with the object questions.
+
+Functions - need to create functions which - gets the questions, clears the questions, populates the answers (with buttons I'm thinking), 
+controls the timer, and displays the end results of right/wrong.
+
+If there is time would like to create assets which frame the story of the trivia game(maybe a villian that you have to defeat by answering correctly)
+*/
